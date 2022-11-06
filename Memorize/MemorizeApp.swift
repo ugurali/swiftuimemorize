8c3441f71738564d81ff8e750a8cb4ee9f747650
Ali Ugur Aker <ali.ugur-aker@ext.elli.eco>
2022-11-06 23:00:09 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ali Ugur Aker on 06.11.22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
