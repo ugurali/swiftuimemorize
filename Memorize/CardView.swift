bbfa3b02fc6b41e892e5b60a65a17b322620d64f
Ali Ugur Aker <ali.ugur-aker@ext.elli.eco>
2023-01-09 21:48:38 +0100

    Added three tabs with card images

---
+++ Memorize/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by Ali Ugur Aker on 06.11.22.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
